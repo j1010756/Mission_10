f623259db5f587994e94bc0250f240e5a5cfc879
jd1010756 <106291794+j1010756@users.noreply.github.com>
2024-03-11 18:57:56 -0600

    first

--- backend/BowlingAPI/obj/Debug/net8.0/BowlingAPI.AssemblyInfo.cs
+++ backend/BowlingAPI/obj/Debug/net8.0/BowlingAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BowlingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aee226224642496501d53b5c63ba2da2ec25c20d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BowlingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BowlingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
